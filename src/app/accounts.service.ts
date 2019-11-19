97812c86ce8183774784ff191fb96633e510143b
Tomek Ciećwierz <tomasz.ciecwierz@gmail.com>
2019-11-19 12:31:49 +0100

    services

---
+++ src/app/accounts.service.ts
@@ -0,0 +1,39 @@
+import {LoggingService} from './logging.service';
+import {EventEmitter, Injectable} from '@angular/core';
+
+@Injectable()
+export class AccountsService {
+
+  statusUpdated = new EventEmitter<string>();
+
+  accounts = [
+    {
+      name: 'Master Account',
+      status: 'active'
+    },
+    {
+      name: 'Testaccount',
+      status: 'inactive'
+    },
+    {
+      name: 'Hidden Account',
+      status: 'unknown'
+    }
+  ];
+
+  constructor(private loggingService: LoggingService) {
+
+  }
+
+  addAccount(name: string, status: string) {
+    this.accounts.push({name: name, status: status});
+    this.loggingService.logStatusChange(status);
+
+  }
+
+  updateAccount(id: number, status: string) {
+    this.accounts[id].status = status;
+    this.loggingService.logStatusChange(status);
+
+  }
+}
